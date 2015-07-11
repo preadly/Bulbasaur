7b2f05c461e607cf8a5851160041f2ab6d2d796d
Magno Costa <magnocosta.br@gmail.com>
2015-07-11 17:00:51 -0300

    Add new folders for organize better

--- lib/bulbasaur.rb
+++ lib/bulbasaur.rb
@@ -1,9 +1,9 @@
 require "nokogiri"
-require "bulbasaur/extract_images_from_youtube"
-require "bulbasaur/extract_images_from_vimeo"
-require "bulbasaur/extract_images_from_html"
-require "bulbasaur/extract_images_from_all_resources"
-require "bulbasaur/normalize_url"
+require "bulbasaur/extracts/extract_images_from_youtube"
+require "bulbasaur/extracts/extract_images_from_vimeo"
+require "bulbasaur/extracts/extract_images_from_html"
+require "bulbasaur/extracts/extract_images_from_all_resources"
+require "bulbasaur/utils/normalize_url"
 
 module Bulbasaur
 
