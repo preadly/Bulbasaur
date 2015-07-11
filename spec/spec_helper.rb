a25073ebc23054a8eeb476361dc81f9db0cb56c1
Magno Costa <magnocosta.br@gmail.com>
2015-07-11 13:10:34 -0300

    Add simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
-require 'bundler/setup'
+if ENV['COVERAGE'] == 'on'
+  require "simplecov"
+  SimpleCov.minimum_coverage 90
+  SimpleCov.start
+end
+
+require "bundler/setup"
 Bundler.setup
 
-require 'bulbasaur'
+require "bulbasaur"
+require "simplecov"
 
 RSpec.configure do |config|
     # some (optional) config here
