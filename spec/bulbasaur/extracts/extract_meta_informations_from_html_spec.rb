0b80eb9142c05042c92ee7c2fed3d30b1890c5d8
Magno Costa <magnocosta.br@gmail.com>
2015-08-13 14:42:18 -0300

    Refactory for default style code

--- spec/bulbasaur/extracts/extract_meta_informations_from_html_spec.rb
+++ spec/bulbasaur/extracts/extract_meta_informations_from_html_spec.rb
@@ -1,18 +1,38 @@
 require "spec_helper"
 
 RSpec.describe Bulbasaur::ExtractMetaInformationsFromHTML do
-  let(:html) { %Q(<head><meta name="description" content="test-description"><meta property="keywords" value="test-keywords"><meta NAME="author" VALUE="test-author"><link rel="canonical" href="test-canonical"></head>) }
-
+  
   subject do
     described_class.new(html).call
   end
+  
+  let(:html) do
+    %Q(
+      <head>
+        <meta name="description" content="test-description">
+        <meta property="keywords" value="test-keywords">
+        <meta NAME="author" VALUE="test-author">
+        <link rel="canonical" href="test-canonical">
+      </head>
+    ) 
+  end
 
-  describe '#call' do
-    it 'should extract meta informations from html' do
-      meta_names = subject.map {|h| h[:name]}
-      meta_values = subject.map {|h| h[:value]}
-      expect(meta_names).to include 'description', 'keywords', 'author', 'canonical'
-      expect(meta_values).to include 'test-description', 'test-keywords', 'test-author', 'test-canonical'
+  describe "#call" do
+
+    let(:meta_names) do
+      subject.map {|h| h[:name]}
+    end
+    
+    let(:meta_values) do
+      subject.map {|h| h[:value]}
+    end
+    
+    it "Does extract meta names informations from html" do
+      expect(meta_names).to include "description", "keywords", "author", "canonical"
+    end
+    
+    it "Does extract meta values informations from html" do
+      expect(meta_values).to include "test-description", "test-keywords", "test-author", "test-canonical"
     end
   end
 end
